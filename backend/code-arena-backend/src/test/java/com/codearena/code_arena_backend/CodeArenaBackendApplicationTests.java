dedc66f009d5008cf0666650acda456bd1637b3d
Stezsz <stephanrlassaponari@gmail.com>
2026-02-25 20:00:53 +0000

    chore(scaffold): add initial project structure

---
+++ backend/code-arena-backend/src/test/java/com/codearena/code_arena_backend/CodeArenaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codearena.code_arena_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeArenaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
