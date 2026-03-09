1fc9bc43654411c6c31dbfde111aaa2998397e05
Tiago Leite <tiagobscleite@gmail.com>
2026-03-09 18:03:17 +0000

    import corrections

--- backend/code-arena-backend/src/test/java/com/codearena/code_arena_backend/auth/service/AuthServiceTest.java
+++ backend/code-arena-backend/src/test/java/com/codearena/code_arena_backend/auth/service/AuthServiceTest.java
@@ -24,7 +24,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 /**
