0588e32397e69ac2bfd3176c4fefaf5f7b00a5b2
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2026-06-02 11:16:49 +0100

    Enable updated notification badge on login

--- backend/src/main/java/com/codearena/code_arena_backend/notification/controller/NotificationController.java
+++ backend/src/main/java/com/codearena/code_arena_backend/notification/controller/NotificationController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.codearena.code_arena_backend.notification.NotificationService;
 import com.codearena.code_arena_backend.notification.dto.NotificationResponse;
-import com.codearena.code_arena_backend.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,7 +12,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,18 +26,18 @@ public class NotificationController {
     private final NotificationService notificationService;
 
     @GetMapping()
-    public Page<NotificationResponse> getNotifications(@AuthenticationPrincipal User userDetails, Pageable pageable) {
-        return notificationService.getNotifications(userDetails.getId(), pageable);
+    public Page<NotificationResponse> getNotifications(Authentication authentication, Pageable pageable) {
+        return notificationService.getNotifications(authentication.getName(), pageable);
     }
 
     @GetMapping("unread")
-    public List<NotificationResponse> getUnreadNotifications(@AuthenticationPrincipal User userDetails) {
-        return notificationService.getUnreadNotifications(userDetails.getId());
+    public List<NotificationResponse> getUnreadNotifications(Authentication authentication) {
+        return notificationService.getUnreadNotifications(authentication.getName());
     }
 
     @PatchMapping("{id}/read")
-    public void markAsRead(@AuthenticationPrincipal User userDetails, @PathVariable Long id) {
-        notificationService.markAsRead(id, userDetails.getUsername());
+    public void markAsRead(Authentication authentication, @PathVariable Long id) {
+        notificationService.markAsRead(id, authentication.getName());
     }
 
 }
